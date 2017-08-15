bb24718655f92b9d29c511bb0d5d44c8fdf8d3f9
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-08-16 00:16:31 +0900

    Fix Chainer::Variable

--- lib/chainer/variable.rb
+++ lib/chainer/variable.rb
@@ -1,27 +1,50 @@
-require 'numo/narray'
-
 module Chainer
   class Variable
-    attr_accessor :data, :name, :grad, :requires_grad, :node
+    attr_accessor :data, :grad, :requires_grad, :node
 
-    def initialize(data:, name: nil, grad: nil, requires_grad: true)
-      validate(data: data) 
-      self.data = [data]
-      self.name = name
-      self.grad = grad
-      self.requires_grad = requires_grad
+    def initialize(data, **kwargs)
+      args = Utils::Argument.parse_kwargs(kwargs, name: nil, grad: nil, requires_grad: true)
+      unless data.is_a?(Numo::NArray)
+        raise TypeError, "Numo::NArray are expected."
+      end
+
+      @data = [data]
+      @grad = args[:grad]
+      @requires_grad = args[:requires_grad]
+      @node = VariableNode.new(variable: self, name: args[:name], grad: args[:grad])
     end
 
     def data
       return @data[0]
     end
 
-    private
+    def name
+      return @node.name
+    end
 
-    def validate(data:)
-      unless data.is_a?(Numo::NArray)
-        raise TypeError, "Numo::NArray are expected."
-      end
+    def name=(n)
+      @node.name = n
+    end
+
+    def label
+      @node.label
+    end
+
+    def creator
+      @node.creator
+    end
+
+    def creator=(func)
+      @node.creator = func
+    end
+
+    def grad
+      @node.grad
+    end
+
+    def grad=(g)
+      @node.set_grad_with_check(g, nil, self)
     end
   end
 end
+
