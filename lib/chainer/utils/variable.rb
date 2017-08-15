c7ae9d7c099a915fee9da46d2476b3bb26d1af90
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-08-16 00:17:01 +0900

    Add Chainer::Utils::Argument, Chainer::Utils::Variable

---
+++ lib/chainer/utils/variable.rb
@@ -0,0 +1,24 @@
+module Chainer
+  module Utils
+    module Variable
+      def self.check_grad_type(func, x, gx)
+        if x.data.nil? || gx.nil?
+          return
+        end
+
+        unless gx.instance_of?(x.class)
+          raise TypeError, "Type of data and grad mismatch\n#{x.class} != #{gx.class}"
+        end
+
+        unless gx.dtype == x.data.dtype
+          raise TypeError, "Dtype of data and grad mismatch\n#{x.class} != #{gx.class}"
+        end
+
+        unless gx.shape == x.data.shape
+          raise TypeError, "Shape of data and grad mismatch\n#{x.class} != #{gx.class}"
+        end
+      end
+    end
+  end
+end
+
