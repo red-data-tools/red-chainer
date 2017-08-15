d32f893b49cfcebae32eb535083e31e4e2a01368
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-08-15 17:40:07 +0900

    Add Variable class

---
+++ lib/chainer/variable.rb
@@ -0,0 +1,23 @@
+require 'numo/narray'
+
+module Chainer
+  class Variable
+    attr_accessor :data, :name, :grad, :requires_grad, :node
+
+    def initialize(data:, name: nil, grad: nil, requires_grad: true)
+      validate(data: data) 
+      self.data = [data]
+      self.name = name
+      self.grad = grad
+      self.requires_grad = requires_grad
+    end
+
+    private
+
+    def validate(data:)
+      unless data.is_a?(Numo::NArray)
+        raise TypeError, "Numo::NArray are expected."
+      end
+    end
+  end
+end
