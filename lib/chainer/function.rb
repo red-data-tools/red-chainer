423f71bcd947ead2ad663a679f9ea7e894095b27
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-08-16 17:29:26 +0900

    support enable_backprop

--- lib/chainer/function.rb
+++ lib/chainer/function.rb
@@ -1,5 +1,12 @@
 module Chainer
   class Function
+
+    attr_reader :rank
+
+    def initialize
+      @rank = 0
+    end
+
     def call(*inputs)
       inputs = inputs.map do |x|
         if x.instance_of?(Variable)
@@ -18,6 +25,28 @@ module Chainer
 
       ret = outputs.map do |y|
         Variable.new(y, requires_grad: requires_grad)
+      end
+
+      if Chainer.configuration.enable_backprop
+        @rank = inputs.map(&:rank).max || 0
+
+        ret.each { |y| y.set_creator(self) }
+
+        @inputs = inputs.map(&:node)
+        @outputs = ret.map { |y| WeakRef.new(y.node) }
+
+        @input_indexes_to_retain = 0...inputs.size if @input_indexes_to_retain.nil?
+        @input_indexes_to_retain.each do |index|
+          inputs[index].retain_data()
+        end
+        remove_instance_variable(:@input_indexes_to_retain)
+
+        unless @output_indexes_to_retain.nil?
+          @output_indexes_to_retain.each do |index|
+            ret[index].retain_data()
+          end
+          remove_instance_variable(:@output_indexes_to_retain)
+        end
       end
       
       if ret.size == 1
