595f213c155cfae3f9ecd8fcfb23e984535c6c5e
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-08-16 17:31:29 +0900

    add require each file

--- lib/chainer.rb
+++ lib/chainer.rb
@@ -1,6 +1,25 @@
+require "weakref"
+
 require "chainer/version"
 
+require 'chainer/configuration'
+require 'chainer/function'
+require 'chainer/variable'
+require 'chainer/variable_node'
+require 'chainer/utils/argument'
+require 'chainer/utils/variable'
+require 'chainer/utils/array'
+require 'chainer/functions/math/basic_math'
+
+require 'numo/narray'
+
 module Chainer
-  # Your code goes here...
+  def self.configure
+    yield(configuration)
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
 end
 
