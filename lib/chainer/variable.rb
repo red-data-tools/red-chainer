51497ea9c5d359c9992a2d5ffc0fcddab795fc4e
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-08-19 22:22:30 +0900

    Add bacward method to Chainer::Variable Add Mul, Sub operator

--- lib/chainer/variable.rb
+++ lib/chainer/variable.rb
@@ -54,12 +54,127 @@ module Chainer
       @node.set_creator(gen_func)
     end
 
+    def backward(retain_grad: false)    
+      return if self.creator.nil?
+
+      if self.data.size == 1 && self.grad.nil?
+        self.grad = self.data.new_ones
+      end
+
+      funcs = [self.creator]
+
+      while func = funcs.pop
+      
+        outputs = func.outputs.map(&:__getobj__)
+        in_data = func.inputs.map(&:data)
+        out_grad = outputs.map { |y| y.nil? ? nil : y.grad }
+
+        func.output_data = outputs.map { |y| y.nil? ? nil : y.data }
+        gxs = func.backward(in_data, out_grad)
+
+        raise unless gxs.size == in_data.size
+
+        unless func.retain_after_backward
+          func.output_data = nil
+        end
+
+        unless retain_grad
+          outputs.each do |y|
+            unless y.nil? || y == @node
+              y.grad = nil
+            end
+          end
+        end
+
+        seen_vars = []
+        need_copy = []
+
+
+        func.inputs.zip(gxs).each do |x, gx|
+          next if gx.nil?
+          next unless x.requires_grad
+
+          Utils::Variable.check_grad_type(func, x, gx)
+
+          id_x = x.object_id
+          if x.creator.nil? # leaf
+            if x.grad.nil?
+              x.grad = gx
+              need_copy << id_x
+            else
+              if need_copy.include?(id_x)
+                x.grad = Utils::Array.force_array(x.grad + gx)
+                need_copy.delete(id_x)
+              else
+                x.grad += gx
+              end
+            end
+          else # not leaf
+            funcs << x.creator
+            if seen_vars.include?(id_x)
+              if need_copy.include?(id_x)
+                x.grad = Utils::Array.force_array(gx + x.grad)
+                need_copy.delete(id_x)
+              else
+                x.grad += gx
+              end
+            else
+              x.grad = gx
+              seen_vars << id_x
+              need_copy << id_x
+            end
+          end
+        end
+      end 
+    end
+
     def +(other)
       if other.instance_of?(Chainer::Variable)
         Functions::Math::Add.new.(*[self, other])
       else
         Functions::Math::AddConstant.new(other).(self)
       end
+    end
+
+    def -(other) 
+      if other.instance_of?(Chainer::Variable)
+        Functions::Math::Sub.new.(*[self, other])
+      else
+        Functions::Math::AddConstant.new(-other).(self)
+      end
+    end
+
+    def *(other)
+      if other.instance_of?(Chainer::Variable)
+        Functions::Math::Mul.new.(*[self, other])
+      else
+        Functions::Math::MulConstant.new(other).(self)
+      end
+    end
+
+    def /(other)
+      if other.instance_of?(Chainer::Variable)
+        Functions::Math::Div.new.(*[self, other])
+      else
+        Functions::Math::MulConstant.new(1 / other).(self)
+      end
+    end
+
+    def **(other) 
+      if other.instance_of?(Chainer::Variable)
+        Functions::Math::PowVarVar.new.(*[self, other])
+      else
+        Functions::Math::PowVarConst.new(other).(self)
+      end
+    end
+
+    def retain_data
+      @node.data = @data[0]
+    end
+
+    def coerce(other)
+      other = self.data.class[*other] if other.kind_of?(Numeric)
+      [Chainer::Variable.new(other, requires_grad: false), self]
     end
   end
 end
