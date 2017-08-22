331a3f6de97951016d829dfe1159f3ee8cfda5f1
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-08-22 09:00:42 +0900

    Add Util::Initializer and require

--- lib/chainer.rb
+++ lib/chainer.rb
@@ -4,12 +4,21 @@ require "chainer/version"
 
 require 'chainer/configuration'
 require 'chainer/function'
+require 'chainer/initializer'
+require 'chainer/initializers/init'
+require 'chainer/initializers/constant'
+require 'chainer/initializers/normal'
+require 'chainer/link'
+require 'chainer/links/connection/linear'
 require 'chainer/variable'
 require 'chainer/variable_node'
 require 'chainer/utils/argument'
+require 'chainer/utils/initializer'
 require 'chainer/utils/variable'
 require 'chainer/utils/array'
 require 'chainer/functions/math/basic_math'
+require 'chainer/functions/connection/linear'
+require 'chainer/parameter'
 
 require 'numo/narray'
 
