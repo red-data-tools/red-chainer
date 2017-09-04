b85f9a9e6ddae428d112ed42a2aedadb292e7af2
hatappi <hatappi@hatappi.me>
2017-09-04 09:01:47 +0900

    Add UpdateRule class

--- lib/chainer/optimizer.rb
+++ lib/chainer/optimizer.rb
@@ -20,4 +20,50 @@ module Chainer
       end
     end
   end
+
+  class UpdateRule
+    attr_reader :state
+
+    def initialize(parent_hyperparam:)
+      @hooks = {}  
+      @state = nil
+      @enabled = true
+      @hyperparam = Chainer::Hyperparameter.new(parent: parent_hyperparam)
+      @t = 0
+    end
+
+    def update(param)
+      return unless @enabled
+
+      @t += 1
+      prepare(param)
+      @hooks.values.each do |hook|
+        hook.call(param)
+      end
+      update_core(param)
+    end
+
+    def update_core(param)
+      # TODO: support GPU
+      update_core_cpu(param)
+    end
+
+    def update_core_cpu
+      raise NotImplementedError
+    end
+
+    def init_state(param)
+      raise NotImplementedError
+    end
+
+    private
+
+    def prepare(param)
+      if @state.nil?
+        @state = {}
+        init_state(param)
+      end
+      @state.select! { |_, v| v.kind_of?(Numo::NArray) }
+    end
+  end
 end
