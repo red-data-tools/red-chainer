10689e91ae02fdc8c01ee525a8ed3266405091b2
hatappi <hatappi@hatappi.me>
2017-09-04 09:02:34 +0900

    Add AdamRule class for Adam update

--- lib/chainer/optimizers/adam.rb
+++ lib/chainer/optimizers/adam.rb
@@ -1,5 +1,44 @@
 module Chainer
   module Optimizers
+    class AdamRule < UpdateRule
+      def initialize(parent_hyperparam: nil, alpha: nil, beta1: nil, beta2: nil, eps: nil)
+        hyperparam = Hyperparameter.new
+        hyperparam.instance_variable_set('@alpha', 0.001)
+        hyperparam.instance_variable_set('@beta1', 0.9)
+        hyperparam.instance_variable_set('@beta2', 0.999)
+        hyperparam.instance_variable_set('@eps', 1e-8)
+
+        super(parent_hyperparam: parent_hyperparam || hyperparam)
+
+        @hyperparam.instance_variable_set('@alpha', alpha) if alpha
+        @hyperparam.instance_variable_set('@beta1', beta1) if beta1
+        @hyperparam.instance_variable_set('@beta2', beta2) if beta2
+        @hyperparam.instance_variable_set('@eps', eps) if eps
+      end
+
+      def init_state(param)
+        @state[:m] = param.data.new_zeros
+        @state[:v] = param.data.new_zeros
+      end
+
+      def update_core_cpu(param)
+        grad = param.grad
+        return if grad.nil?
+
+        hp = @hyperparam
+        m, v = @state[:m], @state[:v]
+
+        m += (1 - hp.beta1) * (grad - m)
+        v += (1 - hp.beta2) * (grad * grad - v)
+        param.data -= lr * m / (Numo::NMath.sqrt(v) + hp.eps)
+      end
+
+      def lr
+        fix1 = 1.0 - @hyperparam.beta1 ** @t
+        fix2 = 1.0 - @hyperparam.beta2 ** @t
+        @hyperparam.alpha * Math.sqrt(fix2) / fix1
+      end
+    end
 
     class Adam < GradientMethod
       def initialize(alpha: nil, beta1: nil, beta2: nil, eps: nil)
@@ -11,7 +50,7 @@ module Chainer
       end
 
       def create_update_rule
-        # TODO
+        AdamRule.new(parent_hyperparam: @hyperparam)
       end
 
       def lr
