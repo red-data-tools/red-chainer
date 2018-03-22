f521561e1cf3e5ca303e5d4d509d7e188c563e00
hatappi <hatappi@hatappi.me>
2018-03-22 22:44:06 +0900

    add BatchNormalization Function and Link

---
+++ lib/chainer/functions/normalization/batch_normalization.rb
@@ -0,0 +1,94 @@
+module Chainer
+  module Functions
+    module Normalization
+      class BatchNormalizationFunction < Chainer::Function
+        attr_reader :running_mean, :running_var
+        # Batch normalization function with fixed statistics.
+        # This is a variant of batch normalization, where the mean and variance
+        # statistics are given by the caller as fixed variables. This is
+        # used on testing mode of the batch normalization layer, where batch
+        # statistics cannot be used for prediction consistency.
+        #
+        # @param [Chainer::Variable] x Input variable.
+        # @param [Chainer::Variable] gamma Scaling parameter of normalized data.
+        # @param [Chainer::Variable] beta Shifting parameter of scaled normalized data.
+        # @param [Chainer::Variable] mean Shifting parameter of input.
+        # @param [Chainer::Variable] var Square of scaling parameter of input.
+        # @param [float] eps Epsilon value for numerical stability.
+        def self.fixed_batch_normalization(x, gamma, beta, mean, var, eps: 2e-5)
+          old_train = Chainer.configuration.train
+          Chainer.configuration.train = false
+          norm = self.new(eps: eps, mean: nil, var: nil, decay: 0.0).(x, gamma, beta, mean, var)
+          Chainer.configuration.train = old_train
+          norm
+        end
+      
+        def initialize(eps: 2e-5, mean: nil, var: nil, decay: 0.9) 
+          @running_mean = mean
+          @running_var = var
+          @eps = eps
+          @mean_cache = nil
+          @decay = decay
+        end
+
+        def forward(inputs)
+          x, gamma, beta = inputs[0], inputs[1], inputs[2]
+          if Chainer.configuration.train
+            if @running_mean.nil?
+              @running_mean = Numo::NArray[*gamma].new_zeros
+              @running_var = Numo::NArray[*gamma].new_zeros
+            else
+              @running_mean = Numo::NArray[*@running_mean]
+              @running_var = Numo::NArray[*@running_var]
+            end
+          elsif inputs.size == 5
+            @fixed_mean = inputs[3]
+            @fixed_var = inputs[4]
+          end
+
+          head_ndim = gamma.ndim + 1
+          gamma_expander = [1] + gamma.shape + [1] * (x.ndim - head_ndim)
+          gamma = gamma.reshape(*gamma_expander)
+          beta_expander = [1] + beta.shape + [1] * (x.ndim - head_ndim)
+          beta = beta.reshape(*beta_expander)
+        
+          if Chainer.configuration.train
+            axis = [0] + (head_ndim...(x.ndim)).to_a
+            mean = x.mean(axis: axis)
+            var = x.var(axis: axis)
+            var += @eps
+          else
+            mean = @fixed_mean
+            var = @fixed_var + @eps
+          end
+
+          @std = Numo::NMath.sqrt(var)
+
+          mean_expander = [1] + mean.shape + [1] * (x.ndim - head_ndim)
+          x_mu = x - mean.reshape(*mean_expander)
+          std_expander = [1] + @std.shape + [1] * (x.ndim - head_ndim)
+          x_mu /= @std.reshape(*std_expander)
+          @x_hat = x_mu
+          y = gamma * @x_hat
+          y += beta
+
+          if Chainer.configuration.train
+            m = x.size.div(gamma.size)
+            adjust = m / [m - 1.0, 1.0].max
+            @running_mean *= @decay
+            temp_ar = Numo::NArray[*mean]
+            temp_ar *= (1 - @decay)
+            @running_mean += temp_ar
+            
+            @running_var *= @decay
+            temp_ar = Numo::NArray[*var]
+            temp_ar *= ((1 - @decay) * adjust)
+            @running_var += temp_ar
+          end
+
+          [y,]
+        end
+      end
+    end
+  end
+end
---
+++ lib/chainer/links/normalization/batch_normalization.rb
@@ -0,0 +1,106 @@
+module Chainer
+  module Links
+    module Normalization 
+      class BatchNormalization < Chainer::Link
+        # Batch normalization layer on outputs of linear or convolution functions.
+        # 
+        # It runs in three modes: training mode, fine-tuning mode, and testing mode.
+        # In training mode, it normalizes the input by *batch statistics*. It also
+        # maintains approximated population statistics by moving averages, which can
+        # be used for instant evaluation in testing mode.
+        # 
+        # In fine-tuning mode, it accumulates the input to compute *population
+        # statistics*. In order to correctly compute the population statistics, a
+        # user must use this mode to feed mini-batches running through whole training dataset.
+        # 
+        # In testing mode, it uses pre-computed population statistics to normalize the input variable.
+        # The population statistics is approximated if it is computed by training mode,
+        # or accurate if it is correctly computed by fine-tuning mode.
+        #
+        # @param [integer or int array] size Size (or shape) of channel dimensions.
+        # @param [float] decay Decay rate of moving average. It is used on training.
+        # @param [float] eps Epsilon value for numerical stability.
+        # @param [Numo::NArray.dtype] dtype Type to use in computing.
+        # @param [boolean] use_gamma If `true`, use scaling parameter. Otherwise, use unit(1) which makes no effect.
+        # @param [boolean] use_beta If `true`, use shifting parameter. Otherwise, use unit(0) which makes no effect.
+        def initialize(size, decay: 0.9, eps: 2e-5, dtype: Numo::Float32, use_gamma: true, use_beta: true, initial_gamma: nil, initial_beta: nil)
+          super()
+          @avg_mean = dtype.zeros(size)
+          register_persistent('avg_mean')
+          @avg_var = dtype.zeros(size)
+          register_persistent('avg_var')
+          @n = 0
+          register_persistent('n')
+          @decay = decay
+          @eps = eps
+
+          init_scope do
+            if use_gamma
+              initial_gamma = 1 if initial_gamma.nil?
+              initial_gamma = Chainer::Initializers.get_initializer(initial_gamma)
+              initial_gamma.dtype = dtype
+              @gamma = Chainer::Parameter.new(initializer: initial_gamma, shape: size)
+            end
+            if use_beta
+              initial_beta = 0 if initial_beta.nil?
+              initial_beta = Chainer::Initializers.get_initializer(initial_beta)
+              initial_beta.dtype = dtype
+              @beta = Chainer::Parameter.new(initializer: initial_beta, shape: size)
+            end
+          end
+        end
+
+        # Invokes the forward propagation of BatchNormalization.
+        # In training mode, the BatchNormalization computes moving averages of
+        # mean and variance for evaluatino during training, and normalizes the input using batch statistics.
+        # @param [Chainer::Variable] x Input variable.
+        # @param [boolean] finetune If it is in the training mode and `finetune` is `True`,
+        # BatchNormalization runs in fine-tuning mode;
+        # it accumulates the input array to compute population statistics for normalization,
+        # and normalizes the input using batch statistics.
+        def call(x, finetune: false)
+          if self.instance_variable_defined?(:@gamma)
+            gamma = @gamma
+          else
+            gamma = Chainer::Variable.new(x.class.ones[*@avg_mean.shape])
+          end
+
+          if self.instance_variable_defined?(:@beta)
+            beta = @beta
+          else
+            beta = Chainer::Variable.new(x.class.ones[*@avg_mean.shape])
+          end
+          
+          if Chainer.configuration.train
+            if finetune
+              @n += 1
+              decay = 1.0 - 1.0 / @n
+            else
+              decay = @decay
+            end
+
+            func = Chainer::Functions::Normalization::BatchNormalizationFunction.new(eps: @eps, mean: @avg_mean, var: @avg_var, decay: decay)
+            ret = func.(x, gamma, beta)
+
+            @avg_mean[false]  = func.running_mean
+            @avg_var[false] = func.running_var
+          else
+            mean = Chainer::Variable(@avg_mean)
+            var = Chainer::Variable(@avg_var)
+            ret = Chainer::Functions::Normalization::BatchNormalizationFunction.fixed_batch_normalization(x, gamma, beta, mean, var, eps: @eps)
+          end
+
+          ret
+        end
+
+        # Resets the population count for collecting population statistics.
+        # This method can be skipped if it is the first time to use the fine-tuning mode.
+        # Otherwise, this method should be called before starting the fine-tuning mode again.
+        def start_finetuning
+          @n = 0
+        end
+      end
+    end
+  end
+end
+
