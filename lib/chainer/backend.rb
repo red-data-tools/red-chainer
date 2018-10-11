08efe553d1709dcdff961e9b0cf6be3d4c2b11a5
sonots <sonots@gmail.com>
2018-10-11 16:15:54 +0900

    Change get_array_module to return Numo or Cumo.

--- lib/chainer/backend.rb
+++ lib/chainer/backend.rb
@@ -1,9 +1,8 @@
 module Chainer
-  # Gets an appropriate one from +Numo::NArray+ or +Cumo::NArray+.
+  # Gets an appropriate one from +Numo::NArray+ or +Cumo::NArray+ from given arrays.
   #
   # @param [Array<Chainer::Variable> or Array<Numo::NArray> or Array<Cumo::NArray>] args Values to determine whether Numo or Cumo should be used.
-  # @return [Numo::NArray] +Cumo::NArray+ or +Numo::NArray+ is returned based on the types of
-  #   the arguments.
+  # @return [Class] +Cumo::NArray+ or +Numo::NArray+ is returned based on the types of the arguments.
   def get_array_module(*args)
     arrays = args.map {|v| v.kind_of?(Chainer::Variable) ? v.data : v }
     if CUDA.available?
@@ -12,4 +11,17 @@ module Chainer
     return Numo::NArray
   end
   module_function :get_array_module
+
+  # Returns true if the argument is either of +Numo::NArray+ or +Cumo::NArray+.
+  #
+  # @param [Object] obj
+  # @return [Boolean]
+  def array?(obj)
+    if CUDA.available?
+      return true if obj.kind_of?(Cumo::NArray)
+    end
+    return true if obj.kind_of?(Numo::NArray)
+    false
+  end
+  module_function :array?
 end
--- test/backend_test.rb
+++ test/backend_test.rb
@@ -3,8 +3,15 @@
 require 'chainer'
 
 class TestCuda < Test::Unit::TestCase
-  def test_get_array_module_for_numpy()
-    assert_equal(Numo::NArray, Chainer::get_array_module(Numo::NArray[]))
-    assert_equal(Numo::NArray, Chainer::get_array_module(Chainer::Variable.new(Numo::NArray[])))
+  def test_get_array_module_for_numo()
+    assert_equal(Numo, Chainer.get_array_module(Numo::NArray[]))
+    assert_equal(Numo, Chainer.get_array_module(Chainer::Variable.new(Numo::NArray[])))
+  end
+
+  if Chainer::CUDA.available?
+    def test_get_array_module_for_cumo()
+      assert_equal(Cumo, Chainer.get_array_module(Cumo::NArray[]))
+      assert_equal(Cumo, Chainer.get_array_module(Chainer::Variable.new(Cumo::NArray[])))
+    end
   end
 end
