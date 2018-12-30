a4ae288b4d57d25785e3d65b0a0eb25086e5b2dc
youchan <youchan01@gmail.com>
2018-12-30 16:41:46 +0900

    Avoid using `to_a`

--- lib/chainer/functions/connection/embed_id.rb
+++ lib/chainer/functions/connection/embed_id.rb
@@ -10,20 +10,21 @@ module Chainer
           self.new(ignore_label: ignore_label).(x, w)
         end
 
-        def forward_cpu(inputs)
+        def forward(inputs)
+          xm = Chainer.get_array_module(*inputs)
           (x, w) = inputs
 
           unless @ignore_label
-            return [Chainer::Utils::Array.take(w, x.to_a, axis: 0)]
+            return [Chainer::Utils::Array.take(w, x, axis: 0)]
           end
 
-          mask = Numo::Bit.cast(x.flatten.to_a.map{|i| i == @ignore_label}).reshape(*x.shape)
+          mask = xm::Bit.cast(x.flatten.to_a.map{|i| i == @ignore_label}).reshape(*x.shape)
           if mask.where.empty?
-            return [Chainer::Utils::Array.take(w, x.to_a, axis: 0)]
+            return [Chainer::Utils::Array.take(w, x, axis: 0)]
           end
           x = x.dup
           x[mask.where] = 0
-          y = Chainer::Utils::Array.take(w, x.to_a, axis: 0).dup
+          y = Chainer::Utils::Array.take(w, x, axis: 0).dup
 
           ndindex = Chainer::Utils::Array.ndindex(y.shape.take(y.shape.size - 1))
           mask.where.each {|i| y[*ndindex[i], true] = 0 }
@@ -31,15 +32,16 @@ module Chainer
           [y]
         end
 
-        def backward_cpu(inputs, grad_outputs)
+        def backward(inputs, grad_outputs)
           (x, w) = inputs
           gy = grad_outputs[0]
           gw = w.class.zeros(w.shape)
           ndindex = Chainer::Utils::Array.ndindex(gw.shape[0, gw.shape.size - 1])
 
-          x.to_a.flatten.zip(gy.reshape(x.size, true).to_a).each do |ix, igy|
+          gy2 = gy.reshape(x.size, true)
+          x.reshape(x.size).each_with_index do |ix, i|
             next if ix == @ignore_label
-            gw[*ndindex[ix], true] = gw[*ndindex[ix], true] + igy
+            gw[*ndindex[ix], true] = gw[*ndindex[ix], true] + gy2[i, true]
           end
 
           [nil, gw]
