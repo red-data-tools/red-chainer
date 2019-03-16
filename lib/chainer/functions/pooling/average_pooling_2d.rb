94e17321f4ffb1eec04f20360c4b057da3b942e5
hatappi <hatappi@hatappi.me>
2019-03-16 09:12:23 +0900

    AveragePooling2D new style

--- lib/chainer/functions/pooling/average_pooling_2d.rb
+++ lib/chainer/functions/pooling/average_pooling_2d.rb
@@ -2,6 +2,8 @@ module Chainer
   module Functions
     module Pooling
       class AveragePooling2D < Pooling2D
+        attr_reader :in_shape, :in_dtype
+
         # Spatial average pooling function.
         #
         # This function acts similarly to :class:`Convolution2D`,
@@ -14,12 +16,11 @@ module Chainer
         # @param [integer] pad Spatial padding width for the input array. `pad=p` and `pad=[p, p]` are equivalent.
         # @return [Chainer::Variable] Output variable
         def self.average_pooling_2d(x, ksize, stride: nil, pad: 0)
-          self.new(ksize, stride: stride, pad: pad, cover_all: false).(x)
+          self.new(ksize, stride: stride, pad: pad, cover_all: false).apply([x])[0]
         end
 
         # Average pooling over a set of 2d planes.
         def forward(x)
-          retain_inputs([])
           @in_shape = x[0].shape
           @in_dtype = x[0].class
 
@@ -29,7 +30,25 @@ module Chainer
           [y]
         end
 
-        def backward(x, gy)
+        def backward(indexes, gy)
+          AveragePooling2DGrad.new(self).apply(gy)
+        end
+      end
+
+      class AveragePooling2DGrad < FunctionNode
+        def initialize(apool2d)
+          @kh = apool2d.kh
+          @kw = apool2d.kw
+          @sy = apool2d.sy
+          @sx = apool2d.sx
+          @ph = apool2d.ph
+          @pw = apool2d.pw
+          @in_shape = apool2d.in_shape
+          @in_dtype = apool2d.in_dtype
+          @apool2d = apool2d
+        end
+
+        def forward(gy)
           h, w  = @in_shape[2..-1]
           shape = gy[0].shape
           shape.insert(2, 1, 1)
@@ -38,6 +57,10 @@ module Chainer
           gx = Chainer::Utils::Conv.col2im(gcol, @sy, @sx, @ph, @pw, h, w)
           gx /= @kh * @kw
           [gx]
+        end
+
+        def backward(indexes, grad_outputs)
+          AveragePooling2D.new([@kh, @kw], stride: [@sy, @sx], pad: [@ph, @pw], cover_all: false).apply(grad_outputs)
         end
       end
     end
