0480455b46cfef85641baad23b6e268693b2686d
sonots <sonots@gmail.com>
2019-04-10 02:36:59 +0900

    s/conv_backward_filter/conv_grad_w/g

--- lib/chainer/functions/connection/convolution_2d_grad_w.rb
+++ lib/chainer/functions/connection/convolution_2d_grad_w.rb
@@ -31,7 +31,7 @@ module Chainer
 
         private def _forward_cudnn(x, gy)
           gy = gy.cast_to(x.class)
-          [x.conv_backward_filter(gy, @w_shape, stride: [@sy, @sx], pad: [@ph, @pw])]
+          [x.conv_grad_w(gy, @w_shape, stride: [@sy, @sx], pad: [@ph, @pw])]
         end
 
         def backward(indexes, grad_outputs)
